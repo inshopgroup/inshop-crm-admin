5895998a34b957abb70df282791d99cf44af76db
Maksym Mukovoz <maksym.mukovoz@netinteractive.pl>
2019-02-24 22:22:06 +0100

    Product show action - supplirs & sell prices tabs

--- src/components/product/CompanyProducts.vue
+++ src/components/product/CompanyProducts.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="table-responsive">
-    <button class="btn btn-primary" @click="create($event.target)">{{$t('company_product.add')}}</button>
-
-    <br>
-    <br>
-
     <table class="table table-striped table-hover">
       <thead>
       <tr>
@@ -15,7 +10,6 @@
         <th>{{$t('company_product.availability')}}</th>
         <th>{{$t('company_product.createdAt')}}</th>
         <th>{{$t('company_product.updatedAt')}}</th>
-        <th style="width: 100px;" />
       </tr>
       </thead>
       <tbody>
@@ -28,13 +22,6 @@
             {{ companyProduct['company']['name'] }}
           </router-link>
         </td>
-
-        <td>
-          <router-link v-if="companyProduct" :to="{name: 'ProductShow', params: { id: companyProduct['product'].id }}">
-            {{ companyProduct['product']['name'] }}
-          </router-link>
-        </td>
-
         <td>
           {{ companyProduct['currency']['name'] }}
         </td>
@@ -50,97 +37,22 @@
         <td>
           {{ moment(companyProduct['updatedAt']).format('DD-MM-YYYY HH:mm') }}
         </td>
-        <td>
-          <div class="btn-group" role="group">
-            <button class="btn btn-info" @click="edit(companyProduct, $event.target)">
-              <span class="fa fa-pencil" aria-hidden="true" />
-              <span class="sr-only">{{$t('edit')}}</span>
-            </button>
-
-            <button class="btn btn-info" @click="deleteItem(companyProduct)">
-              <span class="fa fa-remove" aria-hidden="true" />
-              <span class="sr-only">{{$t('delete')}}</span>
-            </button>
-          </div>
-        </td>
       </tr>
       </tbody>
     </table>
-
-    <!--<modal-company-product-form-->
-        <!--:modal-edit="modalEdit"-->
-        <!--:enable-company="!company"-->
-        <!--:enable-product="!product"-->
-        <!--@companyProductCreated="reloadCompanyProducts()"-->
-    <!--&gt;</modal-company-product-form>-->
   </div>
 </template>
 
 <script>
-  import { mapActions } from 'vuex'
-  // import ModalCompanyProductForm from './ModalCompanyProductForm'
-
   export default {
     name: 'CompanyProducts',
-    // components: {ModalCompanyProductForm},
     props: {
-      company: null,
-      product: null,
       companyProducts: {
         type: Array,
         default: function () {
           return []
         }
       },
-    },
-    data () {
-      return {
-        modalEdit: {
-          title: null,
-          item: {},
-          url: null,
-          method: null
-        }
-      }
-    },
-    methods: {
-      ...mapActions({
-        del: 'company_product/remove',
-      }),
-      deleteItem (item) {
-        if (window.confirm(this.$t('delete_are_you_sure'))) {
-          this.del(item).then(() => {
-            this.$toastr.s(this.$t('companyProduct.deleted', {companyProduct: item.name}), this.$t('deleted'))
-            this.reloadCompanyProducts()
-          })
-        }
-      },
-      create (button) {
-        this.modalEdit = {
-          title: this.$t('company_product.add'),
-          item: {
-            company: this.company,
-            product: this.product,
-          },
-          url: process.env.API_URL + '/company_products',
-          method: 'post'
-        }
-
-        $('#modal-company-product-edit').modal('show')
-      },
-      edit (item, button) {
-        this.modalEdit = {
-          title: this.$t('company_product.edit', {companyProduct: item.name}),
-          item: Object.assign({}, item),
-          url: process.env.API_URL + item.id,
-          method: 'put'
-        }
-
-        $('#modal-company-product-edit').modal('show')
-      },
-      reloadCompanyProducts () {
-        this.$emit('reloadCompanyProducts')
-      }
     }
   }
 </script>
--- src/components/product/ProductSellPrices.vue
+++ src/components/product/ProductSellPrices.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="table-responsive">
-    <button class="btn btn-primary" @click="create($event.target)">{{$t('product_sell_price.add')}}</button>
-
-    <br>
-    <br>
-
     <table class="table table-striped table-hover">
       <thead>
       <tr>
@@ -19,7 +14,6 @@
         <th>{{$t('product_sell_price.active_to')}}</th>
         <th>{{$t('product_sell_price.createdAt')}}</th>
         <th>{{$t('product_sell_price.updatedAt')}}</th>
-        <th style="width: 100px;" />
       </tr>
       </thead>
       <tbody>
@@ -35,94 +29,22 @@
         <td>{{ moment(productSellPrice['activeTo']).format('DD-MM-YYYY HH:mm') }}</td>
         <td>{{ moment(productSellPrice['createdAt']).format('DD-MM-YYYY HH:mm') }}</td>
         <td>{{ moment(productSellPrice['updatedAt']).format('DD-MM-YYYY HH:mm') }}</td>
-        <td>
-          <div class="btn-group" role="group">
-            <button class="btn btn-info" @click="edit(productSellPrice, $event.target)">
-              <span class="fa fa-pencil" aria-hidden="true" />
-              <span class="sr-only">{{$t('edit')}}</span>
-            </button>
-
-            <button class="btn btn-info" @click="deleteItem(productSellPrice)">
-              <span class="fa fa-remove" aria-hidden="true" />
-              <span class="sr-only">{{$t('delete')}}</span>
-            </button>
-          </div>
-        </td>
       </tr>
       </tbody>
     </table>
-
-    <!--<modal-product-sell-price-form-->
-      <!--:modal-edit="modalEdit"-->
-      <!--@productSellPriceCreated="reloadProductSellPrices()"-->
-    <!--&gt;</modal-product-sell-price-form>-->
   </div>
 </template>
 
 <script>
-  import { mapActions } from 'vuex'
-  import ModalProductSellPriceForm from './ModalProductSellPriceForm'
-
   export default {
     name: 'ProductSellPrices',
-    components: {ModalProductSellPriceForm},
     props: {
-      company: null,
-      product: null,
       productSellPrices: {
         type: Array,
         default: function () {
           return []
         }
       },
-    },
-    data () {
-      return {
-        modalEdit: {
-          title: null,
-          item: {},
-          url: null,
-          method: null
-        }
-      }
-    },
-    methods: {
-      ...mapActions({
-        del: 'product_sell_price/remove',
-      }),
-      deleteItem (item) {
-        if (window.confirm(this.$t('delete_are_you_sure'))) {
-          this.del(item).then(() => {
-            this.$toastr.s(this.$t('product_sell_price.deleted', {product_sell_price: item.name}), this.$t('deleted'))
-            this.reloadProductSellPrices()
-          })
-        }
-      },
-      create (button) {
-        this.modalEdit = {
-          title: this.$t('product_sell_price.add'),
-          item: {
-            product: this.product,
-          },
-          url: process.env.API_URL + '/product_sell_prices',
-          method: 'post'
-        }
-
-        $('#modal-product-sell-price-edit').modal('show')
-      },
-      edit (item, button) {
-        this.modalEdit = {
-          title: this.$t('product_sell_price.edit', {product_sell_price: item.name}),
-          item: Object.assign({}, item),
-          url: process.env.API_URL + item.id,
-          method: 'put'
-        }
-
-        $('#modal-product-sell-price-edit').modal('show')
-      },
-      reloadProductSellPrices () {
-        this.$emit('reloadProductSellPrices')
-      }
     }
   }
 </script>
