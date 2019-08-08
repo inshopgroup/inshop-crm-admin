3ccd941cdfd0f90ab545f322f0f363241516dbec
killer71 <magaky71@gmail.com>
2019-08-08 18:18:38 +0300

    пкщгз

--- src/pages/group/Form.vue
+++ src/pages/group/Form.vue
@@ -18,23 +18,49 @@
             label="isActive"
             @formUpdated="updateValue"
           />
-          <div v-for="module in modules" :key="module.id" class="form-group">
-            <h2>{{ $t(module.name.replace(/\s+/g, '_').toLowerCase()) }}</h2>
-            <span
-              v-for="role in module.roles"
-              :key="role.id"
-              style="margin-left: 20px;"
-            >
-              <form-checkbox
-                :id="role['@id']"
-                :item="{ value: item.roleIRIs.includes(role['@id']) }"
-                :errors="errors"
-                property="value"
-                :label="role.name.toLowerCase()"
-                @formUpdated="updateRole"
-              />
-            </span>
-          </div>
+
+          <v-item-group multiple>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex
+                  v-for="module in modules" 
+                  :key="module.id"
+                  xs12
+                  md2
+                >
+                  <v-card outlined>
+                    <v-card-title 
+                      class="justify-space-between fill-height"
+                    >
+                      {{ $t(module.name.replace(/\s+/g, '_').toLowerCase()) }}
+                      <form-checkbox
+                        :item="item"
+                        :errors="errors"
+                        label="All"
+                        @formUpdated="selectAll"
+                      />
+                    </v-card-title>
+                    <v-card-text>
+                      <span
+                        v-for="role in module.roles"
+                        :key="role.id"
+                      >
+                        <form-checkbox
+                          :id="role['@id']"
+                          :item="{ value: item.roleIRIs.includes(role['@id']) }"
+                          :errors="errors"
+                          property="value"
+                          :label="role.name.toLowerCase()"
+                          @formUpdated="updateRole"
+                        />
+                      </span>
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-item-group>
+
         </div>
       </div>
       <item-edit-actions :item="item" entity="Group" path="group" />
@@ -83,6 +109,9 @@ export default {
       reset: 'group/reset',
       getModules: 'module/getItems'
     }),
+    selectAll() {
+      
+    },
     updateValue(property, value) {
       this.$store.commit('group/GROUP_UPDATE_ITEM', { [property]: value })
     },
