f1bb0f0bad636e5b633daf6b768fa61d4eace82f
Maksym Mukovoz <info@inshop.com.ua>
2019-08-09 22:18:49 +0200

    Show actions

--- src/pages/task_status/Show.vue
+++ src/pages/task_status/Show.vue
@@ -1,103 +1,18 @@
 <template>
-  <div>
-    <section class="content-header">
-      <h1>{{ item && item.name }}</h1>
-    </section>
-    <section class="content">
-      <item-errors entity="taskStatus" />
-      <div class="nav-tabs-custom">
-        <ul class="nav nav-tabs">
-          <li class="active">
-            <a href="#general" data-toggle="tab" aria-expanded="false">{{
-              $t('tabs_general')
-            }}</a>
-          </li>
-          <li>
-            <a href="#history" data-toggle="tab" aria-expanded="false">{{
-              $t('tabs_history')
-            }}</a>
-          </li>
-        </ul>
-        <div class="tab-content">
-          <div id="general" class="tab-pane active">
-            <div v-if="item" class="table-responsive">
-              <table class="table table-striped table-hover">
-                <thead>
-                  <tr>
-                    <th width="20%">
-                      {{ $t('field') }}
-                    </th>
-                    <th>{{ $t('value') }}</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{{ $t('name') }}</td>
-                    <td>{{ item.name }}</td>
-                  </tr>
-
-                  <show-row-is-active :item="item" />
-                  <show-row-created-at :item="item" />
-                  <show-row-updated-at :item="item" />
-                  <show-row-updated-by :item="item" />
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div id="history" class="tab-pane">
-            <history
-              :id="parseInt($route.params.id)"
-              :key="historyKey"
-              entity="TaskStatus"
-              path="task_status"
-            />
-          </div>
-        </div>
-      </div>
-      <item-show-actions :item="item" entity="TaskStatus" path="task_status" />
-    </section>
-  </div>
+  <is-show-template :fields="fields" entity="TaskStatus" />
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
-import ItemShowActions from '../../components/layout/ItemShowActions'
-import ItemErrors from '../../components/layout/errors/ItemErrors'
-import History from '../../components/History'
-import ShowRowIsActive from '../../components/Show/ShowRowIsActive'
-import ShowRowCreatedAt from '../../components/Show/ShowRowCreatedAt'
-import ShowRowUpdatedAt from '../../components/Show/ShowRowUpdatedAt'
-import ShowRowUpdatedBy from '../../components/Show/ShowRowUpdatedBy'
-
-export default {
-  components: {
-    ShowRowUpdatedBy,
-    ShowRowUpdatedAt,
-    ShowRowCreatedAt,
-    ShowRowIsActive,
-    History,
-    ItemErrors,
-    ItemShowActions
-  },
-  data() {
-    return {
-      historyKey: 1
+  export default {
+    data() {
+      return {
+        fields: [
+          {
+            value: 'name',
+            type: 'string',
+          },
+        ]
+      }
     }
-  },
-  computed: mapGetters({
-    item: 'taskStatus/item'
-  }),
-  created() {
-    this.getItem(this.$route.params.id)
-  },
-  beforeDestroy() {
-    this.reset()
-  },
-  methods: {
-    ...mapActions({
-      getItem: 'taskStatus/getItem',
-      reset: 'taskStatus/reset'
-    })
   }
-}
 </script>
--- src/pages/vat/Show.vue
+++ src/pages/vat/Show.vue
@@ -1,107 +1,22 @@
 <template>
-  <div>
-    <section class="content-header">
-      <h1>{{ item && item.name }}</h1>
-    </section>
-    <section class="content">
-      <item-errors entity="vat" />
-      <div class="nav-tabs-custom">
-        <ul class="nav nav-tabs">
-          <li class="active">
-            <a href="#general" data-toggle="tab" aria-expanded="false">{{
-              $t('tabs_general')
-            }}</a>
-          </li>
-          <li>
-            <a href="#history" data-toggle="tab" aria-expanded="false">{{
-              $t('tabs_history')
-            }}</a>
-          </li>
-        </ul>
-        <div class="tab-content">
-          <div id="general" class="tab-pane active">
-            <div v-if="item" class="table-responsive">
-              <table class="table table-striped table-hover">
-                <thead>
-                  <tr>
-                    <th width="20%">
-                      {{ $t('field') }}
-                    </th>
-                    <th>{{ $t('value') }}</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{{ $t('name') }}</td>
-                    <td>{{ item.name }}</td>
-                  </tr>
-                  <tr>
-                    <td>{{ $t('value') }}</td>
-                    <td>{{ item.value }}</td>
-                  </tr>
-
-                  <show-row-is-active :item="item" />
-                  <show-row-created-at :item="item" />
-                  <show-row-updated-at :item="item" />
-                  <show-row-updated-by :item="item" />
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div id="history" class="tab-pane">
-            <history
-              :id="parseInt($route.params.id)"
-              :key="historyKey"
-              entity="Vat"
-              path="vat"
-            />
-          </div>
-        </div>
-      </div>
-      <item-show-actions :item="item" entity="Vat" path="vat" />
-    </section>
-  </div>
+  <is-show-template :fields="fields" entity="Vat" />
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
-import ItemShowActions from '../../components/layout/ItemShowActions'
-import ItemErrors from '../../components/layout/errors/ItemErrors'
-import History from '../../components/History'
-import ShowRowIsActive from '../../components/Show/ShowRowIsActive'
-import ShowRowCreatedAt from '../../components/Show/ShowRowCreatedAt'
-import ShowRowUpdatedAt from '../../components/Show/ShowRowUpdatedAt'
-import ShowRowUpdatedBy from '../../components/Show/ShowRowUpdatedBy'
-
-export default {
-  components: {
-    ShowRowUpdatedBy,
-    ShowRowUpdatedAt,
-    ShowRowCreatedAt,
-    ShowRowIsActive,
-    History,
-    ItemErrors,
-    ItemShowActions
-  },
-  data() {
-    return {
-      historyKey: 1
+  export default {
+    data() {
+      return {
+        fields: [
+          {
+            value: 'name',
+            type: 'string',
+          },
+          {
+            value: 'value',
+            type: 'string',
+          },
+        ]
+      }
     }
-  },
-  computed: mapGetters({
-    item: 'vat/item'
-  }),
-  created() {
-    this.getItem(this.$route.params.id)
-  },
-  beforeDestroy() {
-    this.reset()
-  },
-  methods: {
-    ...mapActions({
-      getItem: 'vat/getItem',
-      reset: 'vat/reset'
-    })
   }
-}
 </script>
