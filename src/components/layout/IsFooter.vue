f77bbf9cc400ce971d223df615e8248669f675e7
killer71 <magaky71@gmail.com>
2019-08-13 18:35:31 +0300

    footer test

--- src/components/layout/IsFooter.vue
+++ src/components/layout/IsFooter.vue
@@ -1,14 +1,18 @@
 <template>
-  <v-footer app>
-    <v-layout align-center justify-space-between row mx-3>
-      <section>
+  <v-footer app color="primary" height="50">
+    <v-layout align-center justify-space-between>
+      <div class="white--text">
         {{ fecha.format(new Date(), 'YYYY') }},
-        <a href="https://inshopcrm.com" target="_blank">Inshop CRM</a>, v1.6.0
-      </section>
-      <section>
+        <a href="https://inshopcrm.com" class="white--text" target="_blank">
+          Inshop CRM
+        </a>, v1.6.0
+      </div>
+      <div class="white--text">
         {{ $t('powered_by') }}
-        <a href="https://inshopgroup.com" target="_blank">Inshop Group</a>
-      </section>
+        <a href="https://inshopgroup.com" class="white--text" target="_blank">
+          Inshop Group
+        </a>
+      </div>
     </v-layout>
   </v-footer>
 </template>
