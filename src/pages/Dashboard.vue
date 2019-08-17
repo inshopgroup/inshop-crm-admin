3b18b94c8b7c7e2383b6021df7b337724b699fcc
Maksym Mukovoz <info@inshop.com.ua>
2019-08-17 11:28:28 +0200

    Dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,40 +1,44 @@
 <template>
-  <is-main-template title="dashboard_title">
-    <v-container fluid>
-      <v-layout row>
-        <v-flex md6 sm12 pa-2>
-          <bar-box :title="$t('clients_30')" :data="data.clients_30" />
-        </v-flex>
-        <v-flex md6 sm12 pa-2>
-          <bar-box :title="$t('events_30')" :data="data.events_30" />
-        </v-flex>
-      </v-layout>
+  <v-container fluid>
+    <v-layout justify-space-between mx-5 row>
+      <v-flex>
+        <is-title title="dashboard_title"></is-title>
+      </v-flex>
+    </v-layout>
 
-      <v-layout row>
-        <v-flex md4 sm12 pa-2>
-          <doughnut-box :title="$t('user_7')" :data="data.user_7" />
-        </v-flex>
-        <v-flex md4 sm12 pa-2>
-          <doughnut-box :title="$t('user_30')" :data="data.user_30" />
-        </v-flex>
-        <v-flex md4 sm12 pa-2>
-          <doughnut-box :title="$t('user_365')" :data="data.user_365" />
-        </v-flex>
-      </v-layout>
+    <v-layout row>
+      <v-flex md6 sm12 pa-2>
+        <bar-box :title="$t('clients_30')" :data="data.clients_30" />
+      </v-flex>
+      <v-flex md6 sm12 pa-2>
+        <bar-box :title="$t('events_30')" :data="data.events_30" />
+      </v-flex>
+    </v-layout>
 
-      <v-layout row>
-        <v-flex md4 sm12 pa-2>
-          <doughnut-box :title="$t('user_time_7')" :data="data.user_time_7" />
-        </v-flex>
-        <v-flex md4 sm12 pa-2>
-          <doughnut-box :title="$t('user_time_30')" :data="data.user_time_30" />
-        </v-flex>
-        <v-flex md4 sm12 pa-2>
-          <doughnut-box :title="$t('user_time_365')" :data="data.user_time_365" />
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </is-main-template>
+    <v-layout row>
+      <v-flex md4 sm12 pa-2>
+        <doughnut-box :title="$t('user_7')" :data="data.user_7" />
+      </v-flex>
+      <v-flex md4 sm12 pa-2>
+        <doughnut-box :title="$t('user_30')" :data="data.user_30" />
+      </v-flex>
+      <v-flex md4 sm12 pa-2>
+        <doughnut-box :title="$t('user_365')" :data="data.user_365" />
+      </v-flex>
+    </v-layout>
+
+    <v-layout row>
+      <v-flex md4 sm12 pa-2>
+        <doughnut-box :title="$t('user_time_7')" :data="data.user_time_7" />
+      </v-flex>
+      <v-flex md4 sm12 pa-2>
+        <doughnut-box :title="$t('user_time_30')" :data="data.user_time_30" />
+      </v-flex>
+      <v-flex md4 sm12 pa-2>
+        <doughnut-box :title="$t('user_time_365')" :data="data.user_time_365" />
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
