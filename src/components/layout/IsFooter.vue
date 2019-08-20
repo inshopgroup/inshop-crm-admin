9e745b4ffbf4115d4aa435823e73749a4649eb83
Yurii Salashnyi <yurik.salashnuy@gmail.com>
2019-08-20 11:41:44 +0300

    Footer grid

--- src/components/layout/IsFooter.vue
+++ src/components/layout/IsFooter.vue
@@ -1,26 +1,25 @@
 <template>
-  <v-content>
-    <v-footer :app="app" color="secondary" dark class="caption">
-      <v-layout align-center justify-space-between>
-        <div>
-          {{ fecha.format(new Date(), 'YYYY') }},
-          <a href="https://inshopcrm.com" class="white--text" target="_blank"
-            >Inshop CRM</a
-          >, v1.6.0
-        </div>
-        <div>
-          {{ $t('powered_by') }}
-          <a href="https://inshopgroup.com" class="white--text" target="_blank">
-            Inshop Group
-          </a>
-        </div>
-      </v-layout>
-    </v-footer>
-  </v-content>
+  <v-footer :app="app" color="secondary" dark class="caption" min-height="32">
+    <v-row no-gutters justify="space-between">
+      <v-col cols="12" sm="auto" class="text-center">
+        {{ fecha.format(new Date(), 'YYYY') }},
+        <a href="https://inshopcrm.com" class="white--text" target="_blank">
+          Inshop CRM
+        </a>, v1.6.0
+      </v-col>
+      <v-col cols="12" sm="auto" class="text-center">
+        {{ $t('powered_by') }}
+        <a href="https://inshopgroup.com" class="white--text" target="_blank">
+          Inshop Group
+        </a>
+      </v-col>
+    </v-row>
+  </v-footer>
 </template>
 
 <script>
 import fecha from 'fecha'
+
 export default {
   name: 'FooterSlot',
   props: {
