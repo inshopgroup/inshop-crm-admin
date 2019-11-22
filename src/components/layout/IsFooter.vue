edf24a955c008ae9f5449eb080e515148de874e5
Maksym Mukovoz <info@inshop.com.ua>
2019-11-22 10:42:17 +0100

    Version v1.6.2

--- src/components/layout/IsFooter.vue
+++ src/components/layout/IsFooter.vue
@@ -5,7 +5,7 @@
         {{ fecha.format(new Date(), 'YYYY') }},
         <a href="https://inshopcrm.com" class="white--text" target="_blank">
           Inshop CRM
-        </a>, v1.6.1
+        </a>, v1.6.2
       </v-col>
       <v-col cols="12" sm="auto" class="text-center">
         {{ $t('powered_by') }}
