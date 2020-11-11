7ba57af8b9bbd89a191d1da507a829f89b08140d
Maksym Mukovoz <info@inshop.com.ua>
2020-11-11 13:24:53 +0100

    Decamilize fix

--- src/utils/decamelize.js
+++ src/utils/decamelize.js
@@ -1,5 +1,3 @@
-const xRegExp = require('xregexp')
-
 export default (text, separator) => {
   if (typeof text !== 'string') {
     throw new TypeError('Expected a string')
@@ -7,16 +5,10 @@ export default (text, separator) => {
 
   separator = typeof separator === 'undefined' ? '_' : separator
 
-  const regex1 = xRegExp('([\\p{Ll}\\d])(\\p{Lu})', 'g')
-  const regex2 = xRegExp('(\\p{Lu}+)(\\p{Lu}[\\p{Ll}\\d]+)', 'g')
-
   return (
     text
-      // TODO: Use this instead of `xregexp` when targeting Node.js 10:
-      // .replace(/([\p{Lowercase_Letter}\d])(\p{Uppercase_Letter})/gu, `$1${separator}$2`)
-      // .replace(/(\p{Lowercase_Letter}+)(\p{Uppercase_Letter}[\p{Lowercase_Letter}\d]+)/gu, `$1${separator}$2`)
-      .replace(regex1, `$1${separator}$2`)
-      .replace(regex2, `$1${separator}$2`)
+      .replace(/([\p{Lowercase_Letter}\d])(\p{Uppercase_Letter})/gu, `$1${separator}$2`)
+      .replace(/(\p{Lowercase_Letter}+)(\p{Uppercase_Letter}[\p{Lowercase_Letter}\d]+)/gu, `$1${separator}$2`)
       .toLowerCase()
   )
 }
