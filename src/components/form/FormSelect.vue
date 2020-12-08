4028ee87f1d15c0bfd9756986a2e5e61de1b073d
Maksym Mukovoz <info@inshop.com.ua>
2020-12-08 21:31:59 +0100

    Added FormSelect component

--- src/components/form/FormSelect.vue
+++ src/components/form/FormSelect.vue
@@ -2,19 +2,22 @@
   <div :class="[isInvalid ? 'is-invalid' : '']">
     <div v-if="isInvalid" class="inshop-errors">{{ errors[property] }}</div>
 
-    <v-text-field
+    <v-select
       :value="item[property]"
       :label="$t(label)"
+      :items="options"
+      :item-text="optionLabel"
+      return-object
       outlined
       dense
       @input="$emit('formUpdated', property, $event)"
-    ></v-text-field>
+    ></v-select>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FormInput',
+  name: 'FormSelect',
   props: {
     item: {
       type: Object,
@@ -31,11 +34,40 @@ export default {
     errors: {
       type: Object,
       default: () => ({})
-    }
+    },
+    optionLabel: {
+      type: String,
+      default: 'name'
+    },
+    optionStore: {
+      type: String,
+      required: true
+    },
+    multiple: {
+      type: Boolean,
+      default: false
+    },
   },
   computed: {
     isInvalid() {
       return Object.keys(this.errors).length > 0 && this.errors[this.property]
+    },
+    options() {
+      if (this.$store) {
+        return this.$store.getters[this.optionStore + '/items'] || []
+      }
+
+      return []
+    },
+  },
+  mounted() {
+    this.getOptions()
+  },
+  methods: {
+    getOptions() {
+      if (this.$store) {
+        this.$store.dispatch(this.optionStore + '/getItems')
+      }
     }
   }
 }
--- src/plugins/formComponents.js
+++ src/plugins/formComponents.js
@@ -6,14 +6,16 @@ import FormInput from '@/components/form/FormInput'
 import FormNumber from '@/components/form/FormNumber'
 import FormTextarea from '@/components/form/FormTextarea'
 import FormCheckbox from '@/components/form/FormCheckbox'
+import FormSelect from '@/components/form/FormSelect'
 
 Vue.component('FormInput', FormInput)
 Vue.component('FormNumber', FormNumber)
 Vue.component('FormTextarea', FormTextarea)
 Vue.component('FormCheckbox', FormCheckbox)
+Vue.component('FormSelect', FormSelect)
 
 Object.keys(FormComponents).forEach(name => {
-  if (!['FormInput', 'FormTextarea', 'FormCheckbox', 'FormNumber'].includes(name)) {
+  if (!['FormInput', 'FormTextarea', 'FormCheckbox', 'FormNumber', 'FormSelect'].includes(name)) {
     Vue.component(name, FormComponents[name])
   }
 })
